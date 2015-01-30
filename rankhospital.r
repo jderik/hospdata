bcfe2073b22a1d18919f402a87b19625e1e721d2
unknown <dsilvaj@DSILVAJ2.asiapacific.hpqcorp.net>
2015-01-30 19:47:17 +1100

    commit 04

--- rankhospital.r
+++ rankhospital.r
@@ -15,15 +15,16 @@
 ## If the number given by num is larger than the number of hospitals in that
 ## state, then the function should return NA. Hospitals that do not have data on a particular outcome should
 ## be excluded from the set of hospitals when deciding the rankings.
-function <- rankhospita(state, outcome, num="best")
+rankhospital <- function(state, outcome, num="best")
 {
   dataread <- read.csv("outcome-of-care-measures.csv", colClasses = "character", na.strings=c("Not Available"," "))
   
-  ##Index nos to pick value from outcome file
-  identify<-c(13,19,25)
-  names(identify)<- c("heart attack","heart failure", "pneumonia")
+  ##Index nos to pick value from outcome file. Here 30 day mortality is used.
+  indexer<-c(11,17,23)
+  names(indexer)<- c("heart attack","heart failure", "pneumonia")
   
-  
+  print(num)
+    print(class(num))
   
   if(outcome !="heart attack" & outcome !="heart failure" & outcome !="pneumonia")
   { stop ("Invalid Outcome")}
@@ -32,10 +33,11 @@ function <- rankhospita(state, outcome, num="best")
   { stop ("Invalid State") }
   
   ##Get the  rank values for given state and outcome 
-  ranknumlist<- unique(dataread[dataread[,"State"]==state,identify[outcome]])
-  maxrank<- max(as.double(ranknumlist),na.rm=TRUE)
-  minrank<- min(as.double(ranknumlist),na.rm=TRUE)
+  ranknumlist<- unique(dataread[dataread[,"State"]==state,indexer[outcome]])
+  worstrank<- max(as.double(ranknumlist),na.rm=TRUE)
+  bestrank<- min(as.double(ranknumlist),na.rm=TRUE)
   ranknum<-"NA"
+  hospital<-NA
   
   ##Check that num is 1 of 3 options
   if(num!="best" & num!="worst")
@@ -43,12 +45,23 @@ function <- rankhospita(state, outcome, num="best")
     if(!is.na(suppressWarnings(as.integer(num))))
     {     
       ## Picks unique hospital count from given state
-      x<-unique(dataread[dataread[,"State"]==state,"Hospital.Name"])
+      x<-unique(dataread[dataread[,"State"]==state & !is.na(dataread[,indexer[outcome]]),"Hospital.Name"])
+   
       
-      if(length(x[,1]<=as.integer(num)))
+      if(length(x)>=as.integer(num))
       {
           ranknum<-as.integer(num) 
-        
+          
+          
+          ##GetSubset of Hospital data for the state
+          subsetdata<- dataread[ dataread[,"State"]==state,]
+          ##Sort subset by ascending order of outcome and remove na rows
+          subsetdata2<-subsetdata[(sort(as.integer(subsetdata[,indexer[outcome]]), decreasing=FALSE, na.last=NA)),]
+          
+          
+          ##pick the ranked hospital
+          hospital<- subsetdata2[ranknum, "Hospital.Name"]
+          
           
       }
       else
@@ -56,14 +69,48 @@ function <- rankhospita(state, outcome, num="best")
       
     }
     else
-    { stop("Ranking is not appropriate")}
+    { stop("Ranking is not appropriately provided")}
   }
   else
-  { ranknum<-num}
+  { 
+    if (num=="best")
+      { source("best.r")
+        
+        subsetdata<- dataread[ dataread[,"State"]==state,]
+        
+        ##Get the minimum reading
+        x<-as.double(subsetdata[,indexer[outcome]])
+        minVal <-which.min(x)
+        
+        ##Array of Hospital where the mortality is max for given outcome
+        hospitallist<- subsetdata[ minVal, "Hospital.Name"]
+        hospital<-hospitalist[sort(hospitallist, decreasing=FALSE)]
+        
+      }
+    else
+      if(num=="worst")
+      { 
+        ##GetSubset of Hospital data for the state
+        subsetdata<- dataread[ dataread[,"State"]==state,]
+        
+        ##Get the minimum reading
+        x<-as.double(subsetdata[,indexer[outcome]])
+        maxVal <-which.max(x)
+        
+        ##Array of Hospital where the mortality is max for given outcome
+        hospitallist<- subsetdata[ maxVal, "Hospital.Name"]
+        hospital<-hospitalist[sort(hospitallist, decreasing=TRUE)]
+      }
+  
+  }
+   
+    
+    
+    
+   
+
   
   
-  
-  
-  
+  hospital[1]
   
 }
